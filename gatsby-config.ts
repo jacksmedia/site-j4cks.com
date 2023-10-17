a0461cbdb073571d4505190abb0e575f429c7264
jacksmedia <alex@j4cks.com>
2023-10-16 23:37:26 -0700

    🎛️ metadata adjustments

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -8,14 +8,14 @@ const config: GatsbyConfig = {
     // You can overwrite values here that are used for the SEO component
     // You can also add new values here to query them like usual
     // See all options: https://github.com/LekoArts/gatsby-themes/blob/main/themes/gatsby-theme-jodie/gatsby-config.mjs
-    siteTitle: `Jodie`,
-    siteTitleAlt: `Jodie - Gatsby Starter Portfolio`,
-    siteHeadline: `Jodie - Gatsby Theme from @lekoarts`,
-    siteUrl: `https://jodie.lekoarts.de`,
-    siteDescription: `Image-heavy photography portfolio with colorful accents & customizable pages. Includes adaptive image grids powered by CSS grid and automatic image integration into projects.`,
+    siteTitle: `Jacks.Media`,
+    siteTitleAlt: `Jacks.Media`,
+    siteHeadline: `Jacks.Media midjourney by xJ4cks`,
+    siteUrl: `https://jacks.media`,
+    siteDescription: `midjourney by xJ4cks`,
     siteImage: `/banner.jpg`,
     siteLanguage: `en`,
-    author: `@lekoarts_de`,
+    author: `@xj4cks`,
   },
   trailingSlash: `always`,
   plugins: [
