9d6dfadb80904cd3a54d31cf920d460984fc70e2
jacksmedia <alex@j4cks.com>
2023-10-18 13:42:44 -0700

    :memo: manifest adjusted for PWA install name

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -39,9 +39,9 @@ const config: GatsbyConfig = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `jodie - @lekoarts/gatsby-theme-jodie`,
-        short_name: `jodie`,
-        description: `Image-heavy photography portfolio with colorful accents & customizable pages. Includes adaptive image grids powered by CSS grid and automatic image integration into projects.`,
+        name: `Jacks.Media`,
+        short_name: `jacks.media`,
+        description: `Jacks.Media midjourney by xJ4cks`,
         start_url: `/`,
         background_color: `#ffffff`,
         // This will impact how browsers show your PWA/website
